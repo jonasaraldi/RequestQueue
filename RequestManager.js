0659c07b26a4cd4902c0cda1a6044287cae8757f
Jonas Eduardo Araldi <Pro14@UNI4.local>
2016-04-20 18:15:36 -0300

    Initial commit

---
+++ RequestManager.js
@@ -0,0 +1,50 @@
+(function ($root, $) {
+	"use strict";
+
+	$root.RequestManager = $root.RequestManager || RequestManager;
+
+	function RequestManager(amountOfRequests) {
+		var requestsPending = [];
+		var amountOfRequestProcessing = 0;
+
+		if (!this) {
+			return new RequestManager(amountOfRequests);
+		}
+
+		this.addRequest = function(request) {
+			request = request || {};
+			request.complete = callbackFn(request.complete);
+
+			if (amountOfRequestProcessing < amountOfRequests) {
+				processRequest(request);
+			}
+			else {
+				requestsPending.push(request);
+			}
+		}
+
+		function processRequest(request) {
+			request = request || {};
+			amountOfRequestProcessing++;
+			return $.ajax(request);
+		}
+
+		function callbackFn(fn) {
+			return function (data) {
+				if (fn || fn instanceof Function) {
+					fn(data);
+				}
+				next();
+			}
+		}
+
+		function next() {
+			amountOfRequestProcessing--;
+			if (requestsPending.length > 0) {
+				var request = requestsPending.shift();
+				processRequest(request);
+			}
+		}
+	}
+
+})(window, jQuery);
